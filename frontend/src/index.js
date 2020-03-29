7d923800d7a14d42ffde506acdf9cfd1ac1d77ba
Jordan Mendes Prestes <jordanprestes_94@hotmail.com>
2020-03-29 02:14:38 -0300

    Development of the application's front-end

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
+//Comunicação entre React e o Browser
 import ReactDOM from 'react-dom';
-import './index.css';
+//Importação  do componente App
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+
+//Método para exibir os dados do componente App na tela, dentro da div com ID root
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
